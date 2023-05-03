394b01ba4229f793775e5818b7abb851d43c38ce
Fernando Aceves <FAAceves@gmail.com>
2023-05-03 19:09:21 -0300

    added parser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const PORT = 3434;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 let current = '';
 
@@ -9,7 +13,14 @@ app.get('/getString', (req, res) => {
 });
 
 app.post('/updateString', (req, res) => {
-  const newString = req.body.newString;
+    let newString;
+    try {
+      newString = req.body.newString;
+    } catch (error) {
+      console.error(error);
+      res.status(400).send('Invalid request body');
+      return;
+    }
   current = newString;
   res.send(`Current updated`);
 });
